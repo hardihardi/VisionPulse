c7ec75fb0cbb9b8ef01067e89a410d405172833b
HARDIANTO <hardihrdnt@mhs.pelitabangsa.ac.id>
2025-11-28 02:13:54 +0000

    Pencarian Plat dan Isi Tabel Belum Bisa dari output Video yang di Upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDo_1yB6W-x9Sajji2n13pGN2_CoqX1_oA",
+  authDomain: "visionpulse-14a51.firebaseapp.com",
+  projectId: "visionpulse-14a51",
+  storageBucket: "visionpulse-14a51.appspot.com",
+  messagingSenderId: "983103214436",
+  appId: "1:983103214436:web:ea42144e76a6b579174579",
+  measurementId: "G-PE1M3B09M2"
 };
 
 
